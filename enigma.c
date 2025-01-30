32e14534bc3870345f87db79ece1c15db2e6d3f5
JSneeky <qv24307@bristol.ac.uk>
2025-01-30 12:34:11 +0000

    Starting Position Functionality for Rotors

--- enigma.c
+++ enigma.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+#define EXIT_CODE 1
+
+void error() {
+    fprintf(stderr, "Input error.");
+    exit(EXIT_CODE);
+}
 
 //Standard Input: A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 //Plugboard:      O G V D Z Q B P L X K I N M A H F R T S U C W J Y E
@@ -36,7 +44,7 @@ char rotor(char r, char rotor1[], char rotor2[]) {
 
 //Rotates the input rotor by shifting all elements along by one.
 //Takes place on every key press for rotor I, every 26 key presses for rotor II 
-//and every 676 key presses for rotor III.
+//and every 676 key presses for rotor III. Assuming that the notches are at the starting position.
 //This mimics the mechanical rotation of the rotors within the machine. 
 void rotate(char rotor[]) {
     char store = rotor[25];
@@ -91,14 +99,38 @@ void controller(char input[], char rotor1[], char rotor2[], char rotor3[]) {
     printf("Encryption Complete!!\n%s\n", encrypt);
 }
 
-//Instanciation of the 3 rotors. All rotors have a starting position of 0.
-int main(int n, char *args[n]) {
+//Controls the starting position of the rotors. Set to 0 by default.
+void startingPos(char rotor1[], char rotor2[], char rotor3[], int pos1, int pos2, int pos3) {
+    for (int i = 0; i < pos1; i++) {
+        rotate(rotor1);
+    }
+    for (int i = 0; i < pos2; i++) {
+        rotate(rotor2);
+    }
+    for (int i = 0; i < pos3; i++) {
+        rotate(rotor3);
+    }
+}
+
+void rotorCreate(char input[], int pos1, int pos2, int pos3) {
     char rotor1[27];
     char rotor2[27];
     char rotor3[27];
     strcpy(rotor1, "ekmflgdqvzntowyhxuspaibrcj");
     strcpy(rotor2, "ajdksiruxblhwtmcqgznpyfvoe");
     strcpy(rotor3, "bdfhjlcprtxvznyeiwgakmusqo");
-    controller(args[1], rotor1, rotor2, rotor3);
-    return 0;
+    startingPos(rotor1, rotor2, rotor3, pos1, pos2, pos3);
+    controller(input, rotor1, rotor2, rotor3);
+}
+
+//Instanciation of the 3 rotors. All rotors have a starting position of 0.
+int main(int n, char *args[n]) {
+    if (n == 2) {
+        int pos1 = 0;
+        int pos2 = 0;
+        int pos3 = 0;
+        rotorCreate(args[1], pos1, pos2, pos3);
+        return 0;
+    }
+    else error();
 }
